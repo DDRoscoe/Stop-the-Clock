50da08aab35a6598262cd926026e7b2786b39ad8
Jalen Pastor <48546395+DDRoscoe@users.noreply.github.com>
2024-07-17 23:55:23 -0700

    MENU LINKING STUFF

---
+++ Stop the Clock/Assets/Scripts/ToggleGameView.cs
@@ -0,0 +1,32 @@
+using Doozy.Runtime.UIManager;
+using Doozy.Runtime.UIManager.Components;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGameView : MonoBehaviour
+{
+    public GameObject gameView;
+    //public UIButoon playButton;
+
+    private void OnPressed()
+    {
+
+    }
+
+    public void EnableView()
+    {
+        if (gameView != null)
+            gameView.SetActive(true);
+        else
+            Debug.Log("Object not assigned");
+    }
+
+    public void DisableView()
+    {
+        if (gameView != null)
+        {
+            gameView.SetActive(false);
+        }
+    }
+}
